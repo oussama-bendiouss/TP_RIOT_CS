dd7f9fdecfb7fecf27194baf7b190366d049d8be
Hiet Guillaume <guillaume.hiet@centralesupelec.fr>
2020-04-29 04:32:55 +0200

    ajout squelette

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "thread.h"
+#include "xtimer.h"
+
+
+int main(void)
+{
+    /* bloucle principale */
+    while (1) {
+	    
+        xtimer_sleep(4);
+    } 
+    return 0;
+}
